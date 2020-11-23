825f77f16e58f6f22759ec9c595977e1fdeccc96
Daoud <99m.daoud@gmail.com>
2020-11-23 16:20:10 +0100

    aggiunto bonus 1

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 
 // BONUS 1: Creare una select con tutti i generi dei dischi. In base al genere che viene selezionato nella select, vengono visualizzati i cd corrispondenti.
 // N.B.: per fare questo punto non è necessario modificare l'array dei dati
+
 // BONUS 2: Ordinare i dischi per anno di uscita.
 
 // link api: 'https://flynn.boolean.careers/exercises/api/array/music'
@@ -10,13 +11,21 @@
 const app = new Vue({
     el: '#root',
     data: {
-        songs: []
+        songs: [],
+        genres: [],
+        currentGenre: ''
     },
     mounted() {
         axios.get('https://flynn.boolean.careers/exercises/api/array/music')
             .then(result => {
                 this.songs = result.data.response;
-            });
+
+                this.songs.forEach(song => {
+                    if (!this.genres.includes(song.genre)) {
+                        this.genres.push(song.genre);
+                    }
+            })
+        });
 
     }
 });
